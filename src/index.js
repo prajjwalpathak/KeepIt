1c921bd1702208db0d1f925e22005fa97ecc56e4
prajjwalpathak <prajjwalpathak.asm@gmail.com>
2021-09-07 01:41:54 +0530

    Update Favicon, App.jsx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App";
 
 
 ReactDOM.render(App(), document.getElementById("root"));
